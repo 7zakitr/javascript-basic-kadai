bd613e9554ee333cf893fba84c2be527978d01bb
7zakitr <jakana0612@gmail.com>
2026-09-02 00:57:49 +0900

    ファイルをアップロード

---
+++ kadai_007/calc.js
@@ -0,0 +1,14 @@
+const num = 50;
+
+if (num % 3 === 0) {
+    console.log('3の倍数です');
+}
+if (num % 5 === 0) {
+    console.log('5の倍数です');
+}
+if (num % 3 === 0 && 5 === 0) {
+    console.log('3と5の倍数です');
+}
+else {
+    console.log(num);
+}
